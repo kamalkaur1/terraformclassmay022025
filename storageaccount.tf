8b8b7e3921967e3ec3b05bbb32dae3f21cd61440
Sami Islam <islam.samipkr@gmail.com>
2025-05-05 18:05:47 -0400

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "storage" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.terraformmay022025.name
+  location                 = azurerm_resource_group.terraformmay022025.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
