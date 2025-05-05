a3e81d446ff1076d4c2542b3eaab505bfaf79be5
Sami Islam <islam.samipkr@gmail.com>
2025-05-05 18:12:38 -0400

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,4 +1,4 @@
-resource "azurerm_storage_account" "storage" {
+resource "azurerm_storage_account" "mcitstorage" {
   name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.terraformmay022025.name
   location                 = azurerm_resource_group.terraformmay022025.location
