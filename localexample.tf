58152c53a9f347d1ec901d656bb23e8034108c1a
Sami Islam <islam.samipkr@gmail.com>
2025-05-07 19:37:31 -0400

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -2,6 +2,11 @@ locals{
   servicename="abracadabra"
   forum="abracadabramcit"
   lengthsa=length(local.servicename)
-   winterlistOfSports=["icehockey","snowboarding","iceskating"]
- total_output = ["250", "250", "250"]
+  winterlistOfSports=["icehockey","snowboarding","iceskating"]
+  total_output = ["250", "250", "250"]
+  characters = ["luke", "yoda", "darth"]
+  enemies_destroyed = [4252, 900, 20000056894]
+  character_enemy_map =   { for character in local.characters: # Convert character list to a set
+  character => local.enemies_destroyed
+  }
 }
