939e9fe3fc8225171071227e5e91aa61a3cf7cb5
Sami Islam <islam.samipkr@gmail.com>
2025-05-07 18:46:06 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -8,3 +8,12 @@ output "secondprint"{
 output "lengthsa"{
     value=local.lengthsa
 }
+
+output "wintersportslist"{
+    value=[for sport in local.winterlistOfSports:sport]
+}
+/*
+for 1st element in -->inside the list variable winterlistOfSports--->"icehockey"1st element
+for 2nd element in -->inside the list variable winterlistOfSports--->"snowboarding" 2nd element
+for 3rd element in -->inside the list variable winterlistOfSports--->"iceskating" 3rd element
+*/
