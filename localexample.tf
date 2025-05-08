02f561c52bb798b37fd739e9965be32ea6250af8
Sami Islam <islam.samipkr@gmail.com>
2025-05-07 20:00:13 -0400

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -8,7 +8,7 @@ locals{
   enemies_destroyed = [4252, 900, 20000056894]
   character_enemy_map =   { for character in local.characters: # Convert character list to a set
   character => local.enemies_destroyed
+  }
 
   character_enemy_map_second = zipmap(local.characters, local.enemies_destroyed)
-  }
 }
